15b0d24bd7054a8688c60a38f7359dbdf83cb7a4
Evgeniy <evgnec@gmail.com>
2025-05-15 00:59:54 +0300

    upd

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,22 +1,63 @@
-export const Counter = () => {
-  return (
-    <div className="position-absolute top-50 start-50 translate-middle">
-      <div className="card bg-dark text-white" style={{ width: '600px' }}>
-        <div className="card-body">
-          <h5 className="card-title text-center fs-1">Counter</h5>
-          <p className="card-text text-center" style={{ fontSize: '80px' }}>
-            0
-          </p>
-          <div className="d-flex justify-content-center px-5">
-            <button className="btn btn-outline-success me-5">
-              <i className="bi bi-plus-circle fs-1">+</i>
-            </button>
-            <button className="btn btn-outline-success ms-5">
-              <i className="bi bi-dash-circle fs-1">-</i>
-            </button>
+import { Component } from 'react';
+
+class Counter extends Component {
+  state = {
+    value: 0,
+  };
+  handleClick = (e) => {
+    this.state.value +=1
+  }
+  render() {
+    return (
+      <div className="position-absolute top-50 start-50 translate-middle">
+        <div className="card bg-dark text-white" style={{ width: '600px' }}>
+          <div className="card-body">
+            <h5 className="card-title text-center fs-1">Counter</h5>
+            <p className="card-text text-center" style={{ fontSize: '80px' }}>
+              {this.state.value}
+            </p>
+            <div className="d-flex justify-content-center px-5">
+              <button
+                className="btn btn-outline-success me-5"
+                onClick={this.handleClick}
+              >
+                <i className="bi bi-plus-circle fs-1">+</i>
+              </button>
+              <button className="btn btn-outline-success ms-5">
+                <i className="bi bi-dash-circle fs-1">-</i>
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+// const Counter = () => {
+
+//   function handleClick(e){
+//     console.log(e)
+//   }
+//   return (
+//     <div className="position-absolute top-50 start-50 translate-middle">
+//       <div className="card bg-dark text-white" style={{ width: '600px' }}>
+//         <div className="card-body">
+//           <h5 className="card-title text-center fs-1">Counter</h5>
+//           <p className="card-text text-center" style={{ fontSize: '80px' }}>
+//             0
+//           </p>
+//           <div className="d-flex justify-content-center px-5">
+//             <button className="btn btn-outline-success me-5" onClick={handleClick}>
+//               <i className="bi bi-plus-circle fs-1">+</i>
+//             </button>
+//             <button className="btn btn-outline-success ms-5">
+//               <i className="bi bi-dash-circle fs-1">-</i>
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+export default Counter;
