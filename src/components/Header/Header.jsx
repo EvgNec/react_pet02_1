ef478465ce9152002c3c9aa12e5ae454c89ca4c9
Evgeniy <evgnec@gmail.com>
2025-05-15 00:34:48 +0300

    upd

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return (
+    <div className="navbar bg-dark mb-3">
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1 text-success">Navbar</span>
+        <button className="btn btn-outline-success">Open Modal</button>
+      </div>
+    </div>
+  );
+};
