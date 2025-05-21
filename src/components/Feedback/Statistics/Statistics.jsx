ee79c26459ba5edfa28e5cd350440ad7944c8512
Evgeniy <evgnec@gmail.com>
2025-05-21 22:13:37 +0300

    upd

---
+++ src/components/Feedback/Statistics/Statistics.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Statistics extends Component {
+
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0
+      }
+
+};
+    export default Statistics;
